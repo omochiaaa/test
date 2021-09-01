e8d4887a8e0224f5b84532ba22f245649f431166
山川恵斗 <20c1020051yy@edu.tech.ac.jp>
2021-09-01 10:50:28 +0900

    a

---
+++ zenki_test/zenki_test.cpp
@@ -0,0 +1,75 @@
+﻿// zenki_test.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include<stdio.h>
+#include<string>
+/*
+1) ①　10000001
+　 ②  10111111
+
+2) ① 129
+   ② 95
+
+3) char 1バイト int 4バイト float 4バイト double 8バイト
+
+4) char 1バイト int 4バイト float 4バイト double 8バイト
+
+5) ②
+
+6)
+int main()
+{
+	int A, B, C, D;
+
+	std::cin >> A >> B;
+
+	if (A % 2 == 1)
+	{
+		C = A + B;
+		std::cout<<C;
+	}
+	else
+	{
+		D = A + B;
+		std::cout << D;
+	}
+
+	return 0;
+}
+int main()
+{
+	int A, B, C, D;
+	std::string moji;
+
+
+	bool E = true;
+
+	while (E == true)
+	{
+
+		std::cin >> A >> B;
+
+		if (A % 2 == 1)
+		{
+			C = A + B;
+			std::cout << C;
+		}
+		else
+		{
+			D = A + B;
+			std::cout << D;
+		}
+		if (E == false) {
+			break;
+		}
+		printf("type end");
+		std::cin >> moji;
+		if (moji.compare("end")==0) {
+
+			break;
+		}
+		
+	}
+	return 0;
+}*/
